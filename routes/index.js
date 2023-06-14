818bd131c9eac37b17c01e02363f5fa6adc170d5
ElviraMadamovich <110635987+ElviraMadamovich@users.noreply.github.com>
2023-06-14 19:17:59 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ const routes = require('express').Router();
 
 const users = require('./users');
 const cards = require('./cards');
-
-const { auth } = require('../middlewares/auth');
-const { handleNotFoundError } = require('../middlewares/handleNotFoundError');
+const NotFoundError = require('../utils/errors/NotFoundError');
 
 routes.use('/users', users);
 routes.use('/cards', cards);
 
-routes.use('*', auth, handleNotFoundError);
+routes.use('*', (req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
 
 module.exports = routes;
