6ffcb93910f07bb0339f6e425b7d5f625bda0e56
ElviraMadamovich <110635987+ElviraMadamovich@users.noreply.github.com>
2023-06-14 19:01:46 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,11 @@ const { login, createUser } = require('../controllers/users');
 
 const NotFoundError = require('../utils/errors/NotFoundError');
 
-routes.post('/signin', validateLogin, login);
-routes.post('/signup', validateCreateUser, createUser);
-
 routes.use('/users', users);
 routes.use('/cards', cards);
+
+routes.post('/signin', validateLogin, login);
+routes.post('/signup', validateCreateUser, createUser);
 
 routes.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
